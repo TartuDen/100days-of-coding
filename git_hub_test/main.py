da3d3673dc68c66d51e90c17ae6258dbbd339b4f
Pomogadryg <denver1033@gmail.com>
2023-03-27 20:22:17 +0300

    test of book searcher

--- git_hub_test/main.py
+++ git_hub_test/main.py
@@ -1,1 +1,27 @@
 
+my_list = [
+    {"author": "J.K. Rowling", "books": ["Harry Potter and the Philosopher's Stone", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Prisoner of Azkaban"]},
+    {"author": "Stephen King", "books": ["The Shining", "Carrie", "Misery"]},
+    {"author": "Agatha Christie", "books": ["Murder on the Orient Express", "Death on the Nile", "And Then There Were None"]}
+]
+
+
+def parser_(str_to_search):
+    for a in my_list:
+        if str_to_search.lower()==a["author"].lower():
+            return(a["books"])
+        else:
+            for b in a["books"]:
+                if str_to_search.lower()==b.lower():
+                    return(a["author"])
+    return(False)
+
+
+def searcher(name=None, book=None):
+    search_result=[]
+    if name:
+        print(parser_(name))
+    if book:
+        print(parser_(book))
+
+searcher("Stephen King", "The Shining")
